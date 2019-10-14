493bedb10979081a7aa5acd38f6a8de0cb2a28b9
simon arenas <simon.arenas98@gmail.com>
2019-10-14 17:36:56 -0500

    ignore actualizado

---
+++ screens.js
@@ -0,0 +1,5 @@
+git stash
+git stash list //Ver lista de stash's
+git stash branch Nombre //Pasar cambios a una nueva rama
+git stash pop //Regresa al estado antes de haber hecho el stash (misma rama donde lo hicimos) 
+git stash drop //Borrar stash
